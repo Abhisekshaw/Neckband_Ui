80b0f7d7e6608b2f199a5d3ca04f0f5ed050edb0
DishiMedivirt <dishi.medivirt@gmail.com>
2025-02-03 15:30:33 +0530

    Fix Route

--- src/Component/PRoute/ProtectedRoute.js
+++ src/Component/PRoute/ProtectedRoute.js
@@ -2,11 +2,13 @@ import { useSelector } from "react-redux";
 import { Navigate, Outlet } from "react-router-dom";
 
 function ProtectedRoute() {
-  const token = useSelector((state) => state.auth.token); // Access token from Redux store
-
+  // const token = useSelector((state) => state.auth.token); // Access token from Redux store
+  const token = window.localStorage.getItem("token");
   if (!token) {
     return <Navigate to="/" />;
   }
 
   return <Outlet />;
 }
+
+export default ProtectedRoute
